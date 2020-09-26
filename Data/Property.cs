0b5196f9b6e561cb02d28ec64dccb4dc4a8c7836
zpenn <zachpenn@live.com>
2020-09-26 15:58:33 -0700

    use of BlazorInputFile

--- Data/Property.cs
+++ Data/Property.cs
@@ -1,26 +1,27 @@
 ﻿using Microsoft.Extensions.Localization.Internal;
 using System;
 using System.Collections.Generic;
+using BlazorInputFile;              // for IFileEntry
 
 namespace apartment_app.Data
 {
     public class Property 
     {
+        
+        public string Owner { get; set; }           // Name of the property owner
 
-        public string Owner { get; set; }
+        public string AddressLine1 { get; set; }    // The first address line
 
-        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }    // The second address line
 
-        public string AddressLine2 { get; set; }
+        public int Rent { get; set; }               // The rent number/cost per person
 
-        public int Rent { get; set; }
+        public int Spaces { get; set; }             // Number of total occupant spaces in the property
 
-        public int Spaces { get; set; }
+        public int SpacesAvailable { get; set; }    // Number of spaces available in property
 
-        public int SpacesAvailable { get; set; }
+        public string Description { get; set; }     // Text description of additional property information
 
-        public string Description { get; set; }
-
-        public List<string> FileNames { get; set; } // list of images
+        public IFileListEntry[] FileNames { get; set; } // List of image file names
     }
 }
