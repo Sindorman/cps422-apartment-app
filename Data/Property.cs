11d4ad1b7813e6041933c207ae3f6d5275438a9d
zpenn <zachpenn@live.com>
2020-09-26 14:58:53 -0700

    addProperty and property class

---
+++ Data/Property.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Localization.Internal;
+using System;
+using System.Collections.Generic;
+
+namespace apartment_app.Data
+{
+    public class Property 
+    {
+
+        public string Owner { get; set; }
+
+        public string AddressLine1 { get; set; }
+
+        public string AddressLine2 { get; set; }
+
+        public int Rent { get; set; }
+
+        public int Spaces { get; set; }
+
+        public int SpacesAvailable { get; set; }
+
+        public string Description { get; set; }
+
+        public List<string> FileNames { get; set; } // list of images
+    }
+}
